19d77bfa4acd169d226cf2e0ddf558973991864b
lnewson <lnewson@redhat.com>
2013-02-26 11:48:18 +1000

    Update run-test.sh

--- run-test.sh
+++ run-test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# ZANATA_SERVER=http://zanata-fearless.lab.eng.bne.redhat.com:8080/
-ZANATA_SERVER=http://zanatatest.usersys.redhat.com/zanata/
-REST_SERVER=http://skynet-dev.usersys.redhat.com:8080/TopicIndex/seam/resource/rest
-USERNAME=admin
-TOKEN=b6d7044e9ee3b2447c28fb7c50d86d98
-PROJECT=skynet-topics
-PROJECT_VERSION=1
+ZANATA_SERVER=
+REST_SERVER=
+USERNAME=
+TOKEN=
+PROJECT=
+PROJECT_VERSION=
 MAINCLASS=Main
 DEFAULT_LOCALE=en-US
 MIN_ZANATA_CALL_INTERVAL=0.2
