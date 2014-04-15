f310e531d27ac7346e769b12f3c67faa01f752cb
Lee Newson <lnewson@redhat.com>
2014-04-15 16:16:53 +1000

    Updated the shell scripts to work with the changes for 1.5 compatibility.

--- translation-fix/run.sh
+++ translation-fix/run.sh
@@ -7,19 +7,17 @@ TOKEN=
 PROJECT=
 PROJECT_VERSION=
 MAINCLASS=Main
-DEFAULT_LOCALE=en-US
 MIN_ZANATA_CALL_INTERVAL=0.2
 
 # Get the directory hosting the script. This is important if the script is called from 
 # another working directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-java -DtopicIndex.zanataServer=${ZANATA_SERVER} \
-     -DtopicIndex.zanataUsername=${USERNAME} \
-     -DtopicIndex.zanataToken=${TOKEN} \
-     -DtopicIndex.zanataProject=${PROJECT} \
-     -DtopicIndex.zanataProjectVersion=${PROJECT_VERSION} \
-     -DtopicIndex.skynetServer=${REST_SERVER} \
-     -DtopicIndex.defaultLocale=${DEFAULT_LOCALE} \
-     -DtopicIndex.minZanataCallInterval=${MIN_ZANATA_CALL_INTERVAL} \
-     -jar ${DIR}/target/ZanataTranslationFixTool-0.0.1-SNAPSHOT.jar "$@"
+java -Dpressgang.zanataServer=${ZANATA_SERVER} \
+     -Dpressgang.zanataUsername=${USERNAME} \
+     -Dpressgang.zanataToken=${TOKEN} \
+     -Dpressgang.zanataProject=${PROJECT} \
+     -Dpressgang.zanataProjectVersion=${PROJECT_VERSION} \
+     -Dpressgang.restServer=${REST_SERVER} \
+     -Dpressgang.minZanataCallInterval=${MIN_ZANATA_CALL_INTERVAL} \
+     -jar ${DIR}/target/translation-fix-0.0.1-SNAPSHOT.jar "$@"
